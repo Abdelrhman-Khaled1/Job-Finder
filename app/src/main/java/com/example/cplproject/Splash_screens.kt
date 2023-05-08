bdac98ea043005ba7883e549741e5a9e944d240d
Abdelrhman <kabdo17013@gmail.com>
2023-05-08 21:03:53 +0200

    add first version of splash screen

--- app/src/main/java/com/example/cplproject/Splash_screens.kt
+++ app/src/main/java/com/example/cplproject/Splash_screens.kt
@@ -3,10 +3,9 @@ package com.example.cplproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Splash_screens : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_splash_screens)
     }
 }
