216c44846713206501563876a06734c956c1496d
Abdelrhman <kabdo17013@gmail.com>
2023-05-08 21:18:12 +0200

    go to another page

--- app/src/main/java/com/example/cplproject/MainActivity.kt
+++ app/src/main/java/com/example/cplproject/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.cplproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val btn :Button
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn = findViewById(R.id.btn)
+        btn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, Splash_screens::class.java)
+            startActivity(intent)
+
+        })
 
     }
 }
